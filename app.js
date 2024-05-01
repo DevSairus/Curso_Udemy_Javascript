5d26e0ba33195f9affb8616cc4924e2b68ef7afc
DevSairus <66100678+DevSairus@users.noreply.github.com>
2024-05-01 01:15:18 -0500

    Curso Udemy_Javascript

---
+++ app.js
@@ -0,0 +1,38 @@
+var nombre = 'Sergio';
+var edad = 61;
+
+/*SENTENCIA IF...ELSE
+if (edad < 12) {
+    console.log(nombre + ' es un niño');
+}else  if((edad >11) && (edad <18)){
+    console.log(nombre + ' es un adolescente');
+}else if ((edad > 17) && (edad < 60)){
+    console.log(nombre + ' es un adulto');
+}else{
+    console.log(nombre + ' es un anciano');
+}**************************/
+
+/******************* SENTENCIA SWITCH
+*/
+
+/* *****************************
+var mes = 1;
+
+switch(mes){
+    case 1:
+        console.log('Enero');
+        break;
+    case 2:
+        console.log('Febrero');
+        break;
+    case 3:
+        console.log('Marzo');
+        break;
+    case 4:
+        console.log('Abril');
+        break;
+    default:
+        console.log('Mes no encontrado');
+}*************************
+*/
+
---
+++ functions.js
@@ -0,0 +1,15 @@
+
+/*
+var factorial = function(n){
+    if ((n == 0) || (n == 1))
+        return 1;
+    else
+        return(n * factorial(n - 1));
+}
+
+console.log(factorial(0));
+*/
+
+var nombres = ['Sergio', 'Juan', 'Camilo', 'Luisa'];
+var  frutas = new Array('Pera', 'manzana', 'mora', 'patilla');
+
